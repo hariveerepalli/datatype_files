bfdcb4b087f367a35f3bdd387ca6a85b6da7eb37
hariveerepalli <58967947+hariveerepalli@users.noreply.github.com>
2019-12-19 10:55:49 +0530

    'data_type_stuff'

--- datatype_notes.py
+++ datatype_notes.py
@@ -8,7 +8,7 @@ Python supports  types of data types.
     6. Set
 
 
-## What is variable and use of variables ?
+What is variable and use of variables ?
 1. varables are used to store data and we can reuse.
 2. Using variable we can provide value with descriptive name so it can understood more clearly by the reader and ourselves.
 3. if you share a value across program through varable, it is easy to update value.
